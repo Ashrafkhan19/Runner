20e7190c089835633499726431506b14755786aa
ashraf <ashraf@singsys.com>
2024-09-04 10:52:39 +0530

    unused imports clear

--- build-logic/convention/src/main/java/AndroidLibraryComposeConventionalPlugin.kt
+++ build-logic/convention/src/main/java/AndroidLibraryComposeConventionalPlugin.kt
@@ -1,14 +1,8 @@
 import com.android.build.api.dsl.LibraryExtension
-import com.example.convention.ExtensionType
 import com.example.convention.configureAndroidCompose
-import com.example.convention.configureBuildType
-import com.example.convention.configureKotlinAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
-import org.gradle.kotlin.dsl.kotlin
 
 class AndroidLibraryComposeConventionalPlugin : Plugin<Project> {
     override fun apply(target: Project) {
