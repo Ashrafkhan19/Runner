5fc8adff771ac874034ed7b4556c39a2b33e1c2d
ashraf <=>
2024-09-10 11:44:15 +0530

    implemented token refresh and token access

--- core/data/src/main/java/com/example/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/example/core/data/di/CoreDataModule.kt
@@ -8,7 +8,7 @@ import org.koin.dsl.module
 val coreDataModule = module {
 
     single {
-        HttpClientFactory().build()
+        HttpClientFactory(get()).build()
     }
 
     single<SessionStorage> {
