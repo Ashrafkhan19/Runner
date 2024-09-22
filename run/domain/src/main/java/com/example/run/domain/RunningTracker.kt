d991ceb4455f9e24afda29574e17efb5480dbca7
ashraf <=>
2024-09-22 21:53:52 +0530

    Track location in background

--- run/domain/src/main/java/com/example/run/domain/RunningTracker.kt
+++ run/domain/src/main/java/com/example/run/domain/RunningTracker.kt
@@ -32,7 +32,8 @@ class RunningTracker(
     private val _runData = MutableStateFlow(RunData())
     val runData = _runData.asStateFlow()
 
-    private val isTracking = MutableStateFlow(false)
+    private val _isTracking = MutableStateFlow(false)
+    val isTracking = _isTracking.asStateFlow()
     private val isObservingLocation = MutableStateFlow(false)
 
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -46,7 +47,7 @@ class RunningTracker(
 
     init {
 
-        isTracking
+        _isTracking
             .onEach { isTracking ->
                 if (!isTracking) {
                     val newList = buildList {
@@ -66,7 +67,7 @@ class RunningTracker(
 
         currentLocation
             .filterNotNull()
-            .combineTransform(isTracking) { location, isTracking ->
+            .combineTransform(_isTracking) { location, isTracking ->
                 if (isTracking) emit(location)
             }
             .zip(_elapsedTime) { location, elapsedTime ->
@@ -106,7 +107,7 @@ class RunningTracker(
     }
 
     fun setIsTracking(isTracking: Boolean) {
-        this.isTracking.value = isTracking
+        this._isTracking.value = isTracking
     }
 
     fun startObservingLocation() {
--- run/presentation/src/main/java/com/example/run/presentation/active_run/ActiveRunViewModel.kt
+++ run/presentation/src/main/java/com/example/run/presentation/active_run/ActiveRunViewModel.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.run.domain.RunningTracker
+import com.example.run.presentation.active_run.service.ActiveRunService
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -21,7 +22,12 @@ class ActiveRunViewModel(
     private val runningTracker: RunningTracker
 ) : ViewModel() {
 
-    var state by mutableStateOf(ActiveRunState())
+    var state by mutableStateOf(
+        ActiveRunState(
+            shouldTrack = ActiveRunService.isServiceActive && runningTracker.isTracking.value,
+            hasStartedRunning = ActiveRunService.isServiceActive
+        )
+    )
         private set
 
     private val eventChannel = Channel<ActiveRunEvent>()
@@ -114,6 +120,13 @@ class ActiveRunViewModel(
                     showNotificationRationale = false, showLocationRationale = false
                 )
             }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        if (!ActiveRunService.isServiceActive) {
+            runningTracker.stopObservingLocation()
         }
     }
 }
