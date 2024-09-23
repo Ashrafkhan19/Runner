8ea98b501d1f861bbcf136fb7ed53025771bfafa
ashraf <=>
2024-09-23 10:51:49 +0530

    implement run ui list

---
+++ run/presentation/src/main/java/com/example/run/presentation/run_overview/mappper/RunMapper.kt
@@ -0,0 +1,29 @@
+package com.example.run.presentation.run_overview.mappper
+
+import com.example.core.domain.run.Run
+import com.example.core.presentation.ui.*
+import com.example.run.presentation.run_overview.model.RunUi
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+fun Run.toRunUi(): RunUi {
+    val dateTimeInLocalTime = dateTimeUtc
+        .withZoneSameInstant(ZoneId.systemDefault())
+    val formattedDateTime = DateTimeFormatter
+        .ofPattern("MMM dd, yyyy - hh:mma")
+        .format(dateTimeInLocalTime)
+
+    val distanceKm = distanceMeters / 1000.0
+
+    return RunUi(
+        id = id!!,
+        duration = duration.formatted(),
+        dateTime = formattedDateTime,
+        distance = distanceKm.toFormattedKm(),
+        avgSpeed = avgSpeedKmh.toFormattedKmh(),
+        maxSpeed = maxSpeedKmh.toFormattedKmh(),
+        pace = duration.toFormattedPace(distanceKm),
+        totalElevation = totalElevationMeters.toFormattedMeters(),
+        mapPictureUrl = mapPictureUrl
+    )
+}
