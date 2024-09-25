55a24553c4b339bf39dc845882d7a3fe867b45fc
ashraf <=>
2024-09-25 10:20:56 +0530

    create sync worker

--- run/data/build.gradle.kts
+++ run/data/build.gradle.kts
@@ -9,6 +9,11 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.work)
+
+    implementation(projects.core.domain)
+    implementation(projects.core.database)
+
 
 //    implementation(libs.androidx.core.ktx)
 //    implementation(libs.androidx.appcompat)
